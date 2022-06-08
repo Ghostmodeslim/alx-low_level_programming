51dbf9ed74da9dc6f953b4f747b1b96c77866881
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-06-08 06:01:49 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Main
+ * Return: 0
  */
 int main(void)
 {
-printf("_putchar\n");
-
+char text[] = "_putchar";
+int i = 0;
+int j = sizeof(text) / sizeof(text[0]);
+while (i < (j - 1))
+{
+_putchar(text[i]);
+i++;
+}
+_putchar('\n');
 return (0);
 }
+
+
+
 
 
