21b7221cee5aa7883d44d7ff29fdcb1a69cb942e
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-09-03 02:54:33 -0700

    print add node length free lists

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Add node in the first position
+ * @head: the header of de double linked list
+ * @n: data of n
+ *
+ * Return: double linked list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *tmp, *new;
+
+tmp = *head;
+if (head == NULL)
+return (NULL);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->prev = NULL;
+if (tmp == NULL)
+new->next = NULL;
+else
+{
+new->next = *head;
+tmp->prev = new;
+}
+*head = new;
+return (new);
+}
---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free double link list
+ * @head: double link list
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp = head;
+
+while (head != NULL)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
+}
