0bdd4400a3d71e57840c52ef2696e592b82b97ac
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-09-03 07:14:16 -0700

    crack me up

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the node of index
+ * @head: double link list
+ * @index: the index of nodo
+ *
+ * Return: the double link list in the index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *tmp;
+unsigned int i = 0;
+
+tmp = head;
+if (head == NULL)
+return (NULL);
+while (tmp != NULL)
+{
+if (i == index)
+return (tmp);
+tmp = tmp->next;
+i++;
+}
+
+return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - list the nodes of double linked list
+ * @h: the double linked list
+ *
+ * Return: the number of the nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *tmp = NULL;
+int count = 0;
+
+if (h == NULL)
+return (0);
+tmp = h;
+while (tmp != NULL)
+{
+count += 1;
+tmp = tmp->next;
+}
+return (count);
+}
+
+/**
+ * insert_dnodeint_at_index - insert a new node in ani position in the node
+ * @h: double linked list
+ * @idx: index of the linked list
+ * @n: value in the new node
+ *
+ * Return: new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *tmp = NULL, *new = NULL;
+unsigned int i = 1, n_nodes = 0;
+
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+
+if (idx == 0)
+{
+new = add_dnodeint(h, n);
+return (new);
+}
+
+n_nodes = dlistint_len(*h);
+if (idx == n_nodes)
+{
+new = add_dnodeint_end(h, n);
+return (new);
+}
+
+tmp = *h;
+new->n = n;
+while (i != idx)
+{
+tmp = tmp->next;
+if (tmp == NULL)
+return (NULL);
+i++;
+}
+new->next = tmp->next;
+new->prev = tmp;
+tmp->next = new;
+tmp->next->prev = new; /* Next node in position prev */
+return (new);
+}
