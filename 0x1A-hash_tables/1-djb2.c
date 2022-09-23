128832f8fd5069b003dcc39e3bed5dc2be8ad57e
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-09-23 13:59:38 -0700

    implementing djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,9 +4,8 @@
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
  *
- * Return: the hash
+ * Return: hash value
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
@@ -19,4 +18,3 @@ hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
 return (hash);
 }
-
